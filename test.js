780326eeacd1e9d4afc64d69ff40a464c17bca37
justinAguirreASC6 <justinaguirre130@gmail.com>
2019-07-22 16:03:29 -0400

    Office Hours!

--- test.js
+++ test.js
@@ -1,17 +1,27 @@
 // 1. Declare a number called myNumber that is equal to your age and print it
-
+let myNumber = 17;
+console.log(myNumber);
 
 
 // 2. Declare a string called myName that contains your name and print it
-
+let myName = "Justin";
+console.log(myName);
 
 
 // 3. Declare an array named myArray that contains your age, your first name, and if it's true or false that you're a student
+let x = true;
+let y = false;
+let myArray = [myNumber,myName,x,y];
+if (x == true){
+    console.log("student")
+} else if(y == false){
+    console.log("Bruh u grown")
+}
 
 
 
 // 4. Write and call a function called printHello that prints your name to the console by accessing myArray
-
+function printHello
 
 
 /* 5.
